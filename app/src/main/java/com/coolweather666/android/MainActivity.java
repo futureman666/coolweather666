be3bb82cb9f7c57bfda7e210bc18ce7628599e27
shi <shi@gmail.com>
2019-07-08 10:32:29 +0800

    加入显示天气信息的功能

--- app/src/main/java/com/coolweather666/android/MainActivity.java
+++ app/src/main/java/com/coolweather666/android/MainActivity.java
@@ -1,7 +1,16 @@
 package com.coolweather666.android;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +18,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SharedPreferences prefs= PreferenceManager.getDefaultSharedPreferences(this);
+        /*
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.
+                permission.WRITE_SETTINGS)!= PackageManager.PERMISSION_GRANTED){
+            String[] permissions={"android.permission.WRITE_SETTINGS"};
+            ActivityCompat.requestPermissions(MainActivity.this,permissions,1);
+        }
+        */
+        if (prefs.getString("weather",null)!=null){
+            Intent intent=new Intent(this,WeatherActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
     }
+/*
+    @Override
+    public void onRequestPermissionsResult(int requestCode,  String[] permissions,  int[] grantResults) {
+        switch (requestCode){
+            case 1:
+                if (grantResults.length>0){
+                    for (int result:grantResults){
+                        if (result!=PackageManager.PERMISSION_GRANTED){
+                            Toast.makeText(this,"必须同意所有权限才能使用程序",Toast.LENGTH_LONG).show();
+                            finish();
+                            return;
+                        }
+                    }
+
+                }else {
+                    Toast.makeText(this,"发生未知错误",Toast.LENGTH_LONG).show();
+                    finish();
+                }
+                break;
+            default:
+        }
+    }
+    */
 }
